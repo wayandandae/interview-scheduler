427bf8e252ec4420235b6f9fb69e228ba2917ae9
Jack Kang <jkang0724@hotmail.com>
2023-01-22 00:48:06 -0500

    Modify Appointment component to render Header, Show, and Empty child components conditionally

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,22 @@ import React from "react";
 
 import "./style.scss";
 
-export default function Appointment() {
-  return <article className="appointment"></article>;
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
