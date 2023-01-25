fa5809ebd70ff6ab544af5425a6e496c3b34f5a5
Jack Kang <jkang0724@hotmail.com>
2023-01-25 09:55:41 -0500

    Add mode transition functionalities to use save and erase(delete) interviews

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,18 +5,72 @@ import "./style.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import Status from "./Status";
+import Confirm from "./Confirm";
+
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview).then(() => transition(SHOW));
+  }
+  function erase() {
+    transition(DELETING, true);
+    props.cancelInterview(props.id).then(() => transition(SHOW));
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form
+          onSave={save}
+          onCancel={back}
+          interviewers={props.days.interviewers}
+        />
+      )}
+      {mode === SAVING && <Status message="Saving" />}
+      {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Would you like to delete this interview?"
+          onCancel={back}
+          onConfirm={erase}
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onSave={save}
+          onCancel={back}
+          interviewers={props.interviewers}
+        />
       )}
     </article>
   );
