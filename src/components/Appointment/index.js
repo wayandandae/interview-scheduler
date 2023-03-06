eac703b75c1aadb489dff4d2222f1d9a62b9d66a
Jack Kang <jkang0724@hotmail.com>
2023-03-06 09:38:18 -0500

    Add destroy interview function and fix incomplete prop names

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-
+// style import
 import "./style.scss";
-
+// component import
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
-
+// helper/hook import
 import useVisualMode from "hooks/useVisualMode";
-
+// constants for mode transition
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
@@ -18,40 +18,58 @@ const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  // function to save interview with the passed props
   function save(name, interviewer) {
     const interview = {
       student: name,
       interviewer,
     };
     transition(SAVING);
-    props.bookInterview(props.id, interview).then(() => transition(SHOW));
-  }
-  function erase() {
-    transition(DELETING, true);
-    props.cancelInterview(props.id).then(() => transition(SHOW));
+    props
+      .bookInterview(props.id, interview)
+      .then((resp) => {
+        transition(SHOW);
+      })
+      .catch((error) => transition(ERROR_SAVE, true));
   }
 
+  // fucntion to destroy(delete) selected interview
+  function destroy() {
+    transition(DELETING);
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch((error) => transition(ERROR_DELETE, true));
+  }
+
+  // mode(display) changes depending on different conditions and props passed
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          student={props.interview && props.interview.student}
+          interviewer={props.interview && props.interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
         <Form
           onSave={save}
           onCancel={back}
-          interviewers={props.days.interviewers}
+          student={props.interview && props.interview.student}
+          interviewer={props.interview && props.interview.interviewer}
+          interviewers={props.interviewers}
         />
       )}
       {mode === SAVING && <Status message="Saving" />}
@@ -60,7 +78,7 @@ export default function Appointment(props) {
         <Confirm
           message="Would you like to delete this interview?"
           onCancel={back}
-          onConfirm={erase}
+          onConfirm={destroy}
         />
       )}
       {mode === EDIT && (
