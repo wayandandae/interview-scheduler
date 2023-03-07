4f9f1daf277a9f76ebed55b8d27dfa48ab3f1c7e
Jack Kang <jkang0724@hotmail.com>
2023-03-06 20:06:01 -0500

    Add put and delete axios methods to mock module

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -82,4 +82,18 @@ export default {
       });
     }
   }),
+
+  put: jest.fn((url) => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content",
+    });
+  }),
+
+  delete: jest.fn((url) => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content",
+    });
+  }),
 };
